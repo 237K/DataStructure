132f2c1909c7d3252c3801d10b521a49958ba06c
237K <237ref@gmail.com>
2019-02-26 17:23:46 +0900

    [Circular Linked List] 원형연결리스트 ADT정의 및 구현

--- CircularLinkedList.cpp
+++ CircularLinkedList.cpp
@@ -21,7 +21,7 @@ using namespace std;
 template <typename T>
 class Node
 {
-private:
+public:
 	T data;
 	Node* next;
 };
@@ -38,8 +38,8 @@ public:
 	void Init();
 	void InsertBack(T data);
 	void InsertFront(T data);
-	int First(T* data);
-	int Next(T* data);
+	int First(T* pdata);
+	int Next(T* pdata);
 	T Remove();
 	int Count();
 };
@@ -56,7 +56,7 @@ void CLL<T>::Init()
 template <typename T>
 void CLL<T>::InsertFront(T data)
 {
-	Node<T>* newNode = new Node<T>*;
+	Node<T>* newNode = new Node<T>;
 	newNode->data = data;
 	if (tail == NULL)
 	{
@@ -74,7 +74,7 @@ void CLL<T>::InsertFront(T data)
 template <typename T>
 void CLL<T>::InsertBack(T data)
 {
-	Node<T>* newNode = new Node<T>*;
+	Node<T>* newNode = new Node<T>;
 	newNode->data = data;
 	if (tail == NULL)
 	{
@@ -90,3 +90,122 @@ void CLL<T>::InsertBack(T data)
 	numOfdata++;
 }
 
+template <typename T>
+int CLL<T>::First(T* pdata)
+{
+	if (tail == NULL)
+		return 0;
+	else
+	{
+		cur = tail->next;
+		before = tail;
+		*pdata = cur->data;
+		return 1;
+	}
+}
+
+template <typename T>
+int CLL<T>::Next(T* pdata)
+{
+	if (tail == NULL)
+		return 0;
+	else
+	{
+		before = cur;
+		cur = cur->next;
+		*pdata = cur->data;
+		return 1;
+	}
+}
+
+template <typename T>
+T CLL<T>::Remove()
+{
+	Node<T>* tempNode = cur;
+	T tempData = cur->data;
+
+	if (tempNode == tail)
+	{
+		if (tail == tail->next)
+		{
+			tail = NULL;
+		}
+		else
+		{
+			tail = before;
+		}
+	}
+
+	before->next = cur->next;
+	cur = before;
+
+	delete tempNode;
+	numOfdata--;
+
+	return tempData;
+}
+
+template <typename T>
+int CLL<T>::Count()
+{
+	return numOfdata;
+}
+
+int main(void)
+{
+	CLL<int> cll;
+	int data, i, nodeNum;
+	cll.Init();
+
+	cll.InsertBack(3);
+	cll.InsertBack(4);
+	cll.InsertBack(5);
+
+	cll.InsertFront(2);
+	cll.InsertFront(1);
+
+	cout << "원형연결리스트이므로 무한 반복 출력할 수 있음. 3번 연속 출력" << endl;
+	if (cll.First(&data))
+	{
+		cout << data << ' ';
+		for (i = 0; i < cll.Count() * 3-1; ++i)
+		{
+			if (cll.Next(&data))
+				cout << data << ' ';
+		}
+		cout << endl;
+	}
+	cout << endl<<endl;
+	cout << "=========2의 배수 삭제=========" << endl;
+	nodeNum = cll.Count();
+
+	if (nodeNum != 0)
+	{
+		if (cll.First(&data))
+		{
+			if (data % 2 == 0)
+				cll.Remove();
+		}
+		for (i = 0; i < nodeNum; ++i)
+		{
+			if (cll.Next(&data))
+			{
+				if (data % 2 == 0)
+					cll.Remove();
+			}
+		}
+	}
+
+	if (cll.First(&data))
+	{
+		cout << data << ' ';
+		for (i = 0; i < cll.Count()-1; ++i)
+		{
+			if (cll.Next(&data))
+				cout << data << ' ';
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
